4ae18375092245f122f44ed0a300175ad899d912
Ryohei Ueda <garaemon@gmail.com>
2014-04-23 09:34:17 +0900

    colorize the branch if it is not one of master, hydro-devel and groovy-devel

--- rosenv.sh
+++ rosenv.sh
@@ -406,7 +406,14 @@ catmake() {
 wsinfo_current_branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-  echo ${ref#refs/heads/}
+  branch=${ref#refs/heads/}
+  if [ "$branch" = "master" -o \
+      "$branch" = "hydro-devel" -o \
+      "$branch" = "groovy-devel" ]; then
+      echo $branch
+  else
+      echo -e "\e[1;31m $branch \e[m"
+  fi
 }
 
 
